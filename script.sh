16f058e4443e4d33e5c7ad51254d417c072b5c8c
Ruslan123Rex <134002570+Ruslan123Rex@users.noreply.github.com>
2023-08-27 15:45:19 +0300

    Delete script.sh

--- script.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-git clone https://gitlab.com/staskuznetsov/site.git -b main /home/shtefan_r/git_hub/main
-git clone https://gitlab.com/staskuznetsov/site.git -b dev /home/shtefan_r/git_hub/dev
-git clone https://gitlab.com/staskuznetsov/site.git -b feature /home/shtefan_r/git_hub/featutre
-
-ansible-playbook /home/shtefan_r/git_hub/playbook.yml
-
